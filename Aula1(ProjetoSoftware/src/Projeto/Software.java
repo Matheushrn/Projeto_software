6940331617419e8579d947bb0abe6e02bab23d1c
aluno cmc <alunocmc@umc.br>
2025-02-12 10:47:48 -0300

    Update Software.java
    Luiz é gay

--- Aula1(ProjetoSoftware/src/Projeto/Software.java
+++ Aula1(ProjetoSoftware/src/Projeto/Software.java
@@ -8,6 +8,14 @@ public class Software {
 	
 		System.out.print("Luiz Gay");
 		
+		var Luiz = 20;
+		var Fernando = 10;
+		var Lunando = Luiz + Fernando;
+		
+		
+		System.out.print("\n");
+		System.out.print(Lunando);
+		
 	}
 
 }
