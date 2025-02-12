0d0329f19298a7c68fdeb2d7926d2b838b3774f5
aluno cmc <alunocmc@umc.br>
2025-02-12 10:41:11 -0300

    teste

---
+++ Aula1(ProjetoSoftware/src/Projeto/Software.java
@@ -0,0 +1,13 @@
+package Projeto;
+
+public class Software {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Fds(FinalDeSemana)\n");
+	
+		System.out.print("Luiz Gay");
+		
+	}
+
+}
